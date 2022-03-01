63e284df311056885c373a2deed521538310e210
zahidjos <zahidhasanjos90@gmail.com>
2022-03-01 11:16:04 +0600

    fourt commit

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ const showData=(data)=>{
 let mobileData=data.data;
 const cardRow=document.getElementById('card_row');
 cardRow.textContent=''
+const detailPart=document.getElementById('detail_part');
+detailPart.textContent='';
 if(mobileData.length==0){
     const h3=document.createElement('h3');
     h3.classList.add('text-center');
@@ -32,33 +34,61 @@ for(let i=0; i<20; i++ ){
     let{brand,phone_name,slug,image}=mobileData[i];
     const div=document.createElement('div');
     div.classList.add('col-lg-4');
-    div.innerHTML=` <div class="card mb-3" style="width: 18rem;">
+    div.innerHTML=` <div class="card mb-3 mx-auto" style="width: 18rem;">
     <img src="${image}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${phone_name}</h5>
       <p class="card-text">Brand:${brand}</p>
-      <a href="#" class="btn btn-primary">Details</a>
+      <a href="#" onclick="detailPhone('${slug}')"  class="btn btn-primary">Details</a>
     </div>`
     cardRow.appendChild(div);
 }
 }
 else{
     mobileData.map(singleMobileData=>{
-        console.log(singleMobileData);
+        // console.log(singleMobileData);
         let{brand,phone_name,slug,image}=singleMobileData;
         const div=document.createElement('div');
         div.classList.add('col-lg-4');
-        div.innerHTML=` <div class="card mb-3" style="width: 18rem;">
+        div.innerHTML=` <div class="card mb-3 mx-auto" style="width: 18rem;">
         <img src="${image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone_name}</h5>
           <p class="card-text">Brand:${brand}</p>
-          <a href="#" class="btn btn-primary">Details</a>
+          <a href="#" onclick="detailPhone('${slug}')" class="btn btn-primary">Details</a>
         </div>`
         cardRow.appendChild(div);
     })
+   
+}
+}
+
+const detailPhone=(id)=>{
+const url=`https://openapi.programming-hero.com/api/phone/${id}`;
+// console.log(url);
+fetch(url)
+.then(res=>res.json())
+.then(data=> detailData(data))
 
 }
 
+const detailData=(data)=>{
+    console.log(data);
+let detail=data.data;
+let{brand,image,name,releaseDate,mainFeatures,others}=detail;
+const detailPart=document.getElementById('detail_part');
+detailPart.textContent='';
+const div=document.createElement('div');
+div.innerHTML=`<div class="card mx-auto" style="width:30rem;">
+<img src="${image}" class="card-img-top" alt="...">
+<div class="card-body">
+  
+    <h3>${name}</h3>
+    <p>${brand}</p>
+    <h6>release:${releaseDate}</h6>
+    
+</div>
+</div>`
+detailPart.appendChild(div);
 
 }
